88aeee6c4464bc46b60517c7b0565b86e5d083fa
toohottostop <polezaevkolka@gmail.com>
2021-07-30 21:12:25 +0500

    add model

--- ps_viewer/process_viewer/models.py
+++ ps_viewer/process_viewer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class PsValues(models.Model):
+    pid = models.IntegerField()
+    cpu_usage = models.FloatField()
+    memory_usage = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    command = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'ps_values'
+        ordering = ['pid']
+
+    def __str__(self):
+        return f"{self.pid} {self.cpu_usage} {self.memory_usage} {self.created_at} {self.command}"
